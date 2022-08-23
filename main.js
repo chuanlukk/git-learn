5b9f86ec11316371df85c7b3f3c969a7f03c48b1
chuanlukk <103400281+chuanlukk@users.noreply.github.com>
2022-08-23 15:32:36 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 import App from './App'
+//导入网络请求的包
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http=$http
+//配置请求根路径
+$http.baseUrl='https://api-ugo-web.itheima.net'
+
+//请求拦截器
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+
+//响应拦截器
+$http.afterRequest=function(){
+  uni.hideLoading()
+}
+
+//封装弹窗函数
+uni.$showMsg=function(title='数据请求失败!',duration=1500){
+  return uni.showToast({
+    title,
+    duration,
+    icon: "none"
+  })
+}
 
 // #ifndef VUE3
 import Vue from 'vue'
